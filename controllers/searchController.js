114f9c7b2aca449372d7a10e39f72a852bbb265f
BHUVANA ATMAKURI <110450831+Bhuvana-Atmakuri@users.noreply.github.com>
2025-01-21 22:13:27 +0530

    Update searchController.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,6 +1,6 @@
-const SaelorSeller = require('../models/SaelorSeller.js');
-const ShopifySeller = require('../models/ShopifySeller.js');
-const WooCommerceSeller = require('../models/WooCommerceSeller.js');
+const SaelorSeller = require('../models/saelorSeller.js');
+const ShopifySeller = require('../models/shopifySeller.js');
+const WooCommerceSeller = require('../models/woocommerceSeller.js');
 const { searchProductInShop } = require('../services/saleorService.js');
 const { searchProductInShopify } = require('../services/shopifyService.js');
 const WooCommerceService = require('../services/wooCommerceService.js');  // Import the service class
